6bd3c10dad2794c809f07cd39b273fe533781ac4
Ana Leticia <270024046+oliveiravidal-blip@users.noreply.github.com>
2026-08-14 12:51:26 +0000

    Primeiro aplicativo c++

---
+++ lista_01/ex01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nome;
+    cout << "Digite seu nome: ";
+    cin >> nome;
+    cout << "Olá," << nome << endl;
+
+    return 0;
+}
